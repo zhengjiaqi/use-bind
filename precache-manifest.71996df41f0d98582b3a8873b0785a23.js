14ad23ac045e4276a3cf9d83656b081d2e85d11e
zhengjiaqibj <zhengjiaqibj@yuanfudao.com>
2021-06-14 18:14:42 +0800

    Updates

---
+++ precache-manifest.71996df41f0d98582b3a8873b0785a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4521b0746675a68528931ae40fe92a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b2774e2191360de2a2",
+    "url": "/static/css/main.534f91fc.chunk.css"
+  },
+  {
+    "revision": "f7da298601439a351b02",
+    "url": "/static/js/2.273e21f6.chunk.js"
+  },
+  {
+    "revision": "d376e722e180369e5275",
+    "url": "/static/js/3.82bfcd13.chunk.js"
+  },
+  {
+    "revision": "14b2774e2191360de2a2",
+    "url": "/static/js/main.750005bd.chunk.js"
+  },
+  {
+    "revision": "e52cda8e46254221a4c9",
+    "url": "/static/js/runtime~main.0a718788.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
